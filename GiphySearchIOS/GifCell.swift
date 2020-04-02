36c51e17707682fa7b1c3a0159f91fac08ae70ce
Cao Mai <caomai@rocketmail.com>
2020-04-01 18:14:23 -0700

    [add] gif cell class

---
+++ GiphySearchIOS/GifCell.swift
@@ -0,0 +1,22 @@
+//
+//  GifCell.swift
+//  GiphySearchIOS
+//
+//  Created by Cao Mai on 4/1/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class GifCell: UITableViewCell {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Add views
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
