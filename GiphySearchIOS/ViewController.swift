eb7a0dbe9682c956385d422d95dc9cc4c07b6f7e
Cao Mai <caomai@rocketmail.com>
2020-04-01 18:06:29 -0700

    [add] tableview and search to storyboard

---
+++ GiphySearchIOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GiphySearchIOS
+//
+//  Created by Cao Mai on 4/1/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
